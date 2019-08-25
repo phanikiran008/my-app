c785afbb0a83d9f59a4a3b5bc687d7edaf42c7e7
Phani Kiran <phanikiran008@gmail.com>
2019-08-25 20:11:10 +0530

    Simple Maven

---
+++ src/main/java/com/pkr/app/App.java
@@ -0,0 +1,13 @@
+package com.pkr.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
